17d1e02202a1acffded1ec0275c7e26a26b9e435
Ritu <Ritu@DESKTOP-6BBA7BK>
2026-01-11 17:40:55 +0530

    commit 1- added java program

---
+++ DA1_23MIC0041/src/com/da1/DA1.java
@@ -0,0 +1,9 @@
+package com.da1;
+
+public class DA1 {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Ritu Srivastava");
+        System.out.println("Register Number: 23mic0041");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
